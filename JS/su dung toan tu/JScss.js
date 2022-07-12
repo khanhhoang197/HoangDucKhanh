af029e63bb8e83e8c49f2227b9bc4a9e9edf0295
khanhhoang197 <khanhhoang197.wbs@gmail.com>
2022-07-12 14:50:25 +0700

    bài tập sử dụng toán tử

---
+++ JS/su dung toan tu/JScss.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt('Nhập chiều rộng của hình chữ nhật:');
+inputheight = prompt('Nhập chiều rộng của hình chữ nhật: ');
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write('Diện tích của hình chữ nhật là: ' + area);
